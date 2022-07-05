03c3d1ff47c2c95a78ef9060738b1bc2213028cb
Matthew Williams <mattwilliamsdev@gmail.com>
2022-07-05 10:43:33 +0100

    Added the algos I have done so far

---
+++ merge-sort.js
@@ -0,0 +1,36 @@
+// Merges two already sorted arrays
+function merge(arr1, arr2) {
+  let results = [];
+  // arr1 index
+  let i = 0;
+  // arr2 index
+  let j = 0;
+  // while there is still data to look at in both arrays
+  while (i < arr1.length && j < arr2.length) {
+    if (arr2[j] > arr1[i]) {
+      results.push(arr1[i]);
+      i++;
+    } else {
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+  // while there is still data left in one of the arrays
+  while (i < arr1.length) {
+    results.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++;
+  }
+  return results;
+}
+
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+}
---
+++ recursion.js
@@ -0,0 +1,153 @@
+// Write a function that accepts a string and returns it in reverse
+function reverse(str) {
+  if (str.length <= 1) return str;
+  return reverse(str.slice(1)) + str[0];
+}
+
+// Write a function that accepts a string and returns if it is a pallandrome or not
+function isPalindrome(str) {
+  if (str.length === 1) return true;
+  if (str.length === 2) return str[0] === str[1];
+  if (str[0] === str.slice(-1)) return isPalindrome(str.slice(1, -1));
+  return false;
+}
+
+// Write a function that acceps an array and a callback and checks if at least one
+// value in the array returns true when passed to the callback
+const isOdd = (val) => val % 2 !== 0;
+
+function someRecursive(array, callback) {
+  if (array.length === 0) return false;
+  if (callback(array[0])) return true;
+  return someRecursive(array.slice(1), callback);
+}
+
+// Write a function that acceps an array of arrays and returns a new array with all
+// the values flattened
+function flatten(oldArr) {
+  let newArr = [];
+  for (let i = 0; i < oldArr.length; i++) {
+    if (Array.isArray(oldArr[i])) {
+      newArr = newArr.concat(flatten(oldArr[i]));
+    } else {
+      newArr.push(oldArr[i]);
+    }
+  }
+  return newArr;
+}
+
+// Write a function that acceps an array of strings and capialises the first letter of each
+function capitalizeFirst(array) {
+  if (array.length === 1) {
+    return [array[0][0].toUpperCase() + array[0].substr(1)];
+  }
+  const res = capitalizeFirst(array.slice(0, -1));
+  const string =
+    array.slice(array.length - 1)[0][0].toUpperCase() +
+    array.slice(array.length - 1)[0].substr(1);
+  res.push(string);
+  return res;
+}
+
+// Write a function that returns the sum of all even numbers in an object which may
+// contain nested objects
+let obj1 = {
+  outer: 2,
+  obj: {
+    inner: 2,
+    otherObj: {
+      superInner: 2,
+      notANumber: true,
+      alsoNotANumber: "yup",
+    },
+  },
+};
+
+let obj2 = {
+  a: 2,
+  b: { b: 2, bb: { b: 3, bb: { b: 2 } } },
+  c: { c: { c: 2 }, cc: "ball", ccc: 5 },
+  d: 1,
+  e: { e: { e: 2 }, ee: "car" },
+};
+
+function nestedEvenSum(obj, sum = 0) {
+  for (var key in obj) {
+    if (typeof obj[key] === "object") {
+      sum += nestedEvenSum(obj[key]);
+    } else if (typeof obj[key] === "number" && obj[key] % 2 === 0) {
+      sum += obj[key];
+    }
+  }
+  return sum;
+}
+
+// Write a function that acceps an array of strings and returns a new array of
+// the strings all capitalised
+function capitalizeWords(array) {
+  if (array.length === 1) {
+    return [array[0].toUpperCase()];
+  }
+  let res = capitalizeWords(array.slice(0, -1));
+  res.push(array.slice(array.length - 1)[0].toUpperCase());
+  return res;
+}
+
+// Write a function that acceps an object and finds all the values that are numbers
+// and converts them to strings.
+let obj = {
+  num: 1,
+  test: [],
+  data: {
+    val: 4,
+    info: {
+      isRight: true,
+      random: 66,
+    },
+  },
+};
+
+function stringifyNumbers(obj) {
+  var newObj = {};
+  for (var key in obj) {
+    if (typeof obj[key] === "number") {
+      newObj[key] = obj[key].toString();
+    } else if (typeof obj[key] === "object" && !Array.isArray(obj[key])) {
+      newObj[key] = stringifyNumbers(obj[key]);
+    } else {
+      newObj[key] = obj[key];
+    }
+  }
+  return newObj;
+}
+
+// Write a function that accepts an object and returns all the strings from the object
+// in an array
+const object = {
+  stuff: "foo",
+  data: {
+    val: {
+      thing: {
+        info: "bar",
+        moreInfo: {
+          evenMoreInfo: {
+            weMadeIt: "baz",
+          },
+        },
+      },
+    },
+  },
+};
+
+function collectStrings(obj) {
+  var stringsArr = [];
+  for (var key in obj) {
+    if (typeof obj[key] === "string") {
+      stringsArr.push(obj[key]);
+    } else if (typeof obj[key] === "object") {
+      stringsArr = stringsArr.concat(collectStrings(obj[key]));
+    }
+  }
+
+  return stringsArr;
+}
