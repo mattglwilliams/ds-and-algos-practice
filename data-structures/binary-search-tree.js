0ec9c4a9b9cd202f44048c1e44a76c49c2376b24
Matthew Williams <mattwilliamsdev@gmail.com>
2022-07-21 10:47:45 +0100

    Changed var to let

--- data-structures/binary-search-tree.js
+++ data-structures/binary-search-tree.js
@@ -11,12 +11,12 @@ class BinarySearchTree {
     this.root = null;
   }
   insert(value) {
-    var newNode = new Node(value);
+    let newNode = new Node(value);
     if (this.root === null) {
       this.root = newNode;
       return this;
     }
-    var current = this.root;
+    let current = this.root;
     while (true) {
       if (value === current.value) return undefined;
       if (value < current.value) {
@@ -36,7 +36,7 @@ class BinarySearchTree {
   }
   find(value) {
     if (this.root === null) return false;
-    var current = this.root,
+    let current = this.root,
       found = false;
     while (current && !found) {
       if (value < current.value) {
@@ -52,7 +52,7 @@ class BinarySearchTree {
   }
   contains(value) {
     if (this.root === null) return false;
-    var current = this.root,
+    let current = this.root,
       found = false;
     while (current && !found) {
       if (value < current.value) {
